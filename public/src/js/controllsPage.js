43c3a93314e521f44780c05b9807f7729731ef50
vitorgabrieldev <vitorgabrieldeoliveiragit@gmail.com>
2023-02-04 11:48:02 -0300

    Pirmary Page Done

---
+++ public/src/js/controllsPage.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', () => settingsPage.transitionPagePrimary());
+
+const settingsPage = {
+    transitionPagePrimary: function() {
+        setTimeout(() => {
+            document.querySelector('#pagePrimary').style.marginRight = '200%';
+        }, 4900);                
+    },
+};
