99b4870f1cba050269b18979299a0e4856bf157a
vitorgabrieldev <vitorgabrieldeoliveiragit@gmail.com>
2023-02-04 15:54:42 -0300

    Page Third

--- public/src/js/controllsPage.js
+++ public/src/js/controllsPage.js
@@ -1,16 +1,24 @@
 window.addEventListener('load', () => settingsPage.transitionPagePrimary());
+document.querySelector('#btnNextPage').addEventListener('click', () => settingsPage.transitionPageThird());
 
 // DOM pages
 let pageOne = document.querySelector('#pagePrimary');
 let pageTwo = document.querySelector('#pageSecond');
+let pageThird = document.querySelector('#pageThird');
 
 const settingsPage = {
     transitionPagePrimary: function() {
         setTimeout(() => {
             pageOne.style.marginRight = '200%';
             setTimeout(() => {
-            pageTwo.style.display = "block";
+                pageTwo.style.display = "block";
             },500);
         }, 4900);                
+    },
+    transitionPageThird: function() {
+        pageTwo.style.marginRight = '200%';
+        setTimeout(() => {
+            pageThird.style.display = "block";
+        },500);
     },
 };
