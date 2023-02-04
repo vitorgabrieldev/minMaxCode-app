60c3ef4109caff5db5f60927262954ce2380413c
vitorgabrieldev <vitorgabrieldeoliveiragit@gmail.com>
2023-02-04 14:55:50 -0300

    teste href locate js

--- public/src/js/controllsPage.js
+++ public/src/js/controllsPage.js
@@ -1,9 +1,16 @@
-// window.addEventListener('load', () => settingsPage.transitionPagePrimary());
+window.addEventListener('load', () => settingsPage.transitionPagePrimary());
 
-// const settingsPage = {
-//     transitionPagePrimary: function() {
-//         setTimeout(() => {
-//             document.querySelector('#pagePrimary').style.marginRight = '200%';
-//         }, 4900);                
-//     },
-// };
+// DOM pages
+let pageOne = document.querySelector('#pagePrimary');
+let pageTwo = document.querySelector('#pageSecond');
+
+const settingsPage = {
+    transitionPagePrimary: function() {
+        setTimeout(() => {
+            pageOne.style.marginRight = '200%';
+            setTimeout(() => {
+            pageTwo.style.display = "block";
+            },500);
+        }, 4900);                
+    },
+};
