f2389c0217a33c2bdaa453e596bf4eed79eac87c
Vitor Gabriel de Oliveira <106203763+vitorgabrieldev@users.noreply.github.com>
2023-05-05 22:47:54 -0300

    Update controllsPage.js

--- public/src/js/controllsPage.js
+++ public/src/js/controllsPage.js
@@ -1,25 +1,25 @@
-// window.addEventListener('load', () => settingsPage.transitionPagePrimary());
-// document.querySelector('#btnNextPage').addEventListener('click', () => settingsPage.transitionPageThird());
+window.addEventListener('load', () => settingsPage.transitionPagePrimary());
+document.querySelector('#btnNextPage').addEventListener('click', () => settingsPage.transitionPageThird());
 
 // // DOM pages
-let pageOne = document.querySelector('#pagePrimary').style.display = 'none'
+let pageOne = document.querySelector('#pagePrimary').style.display = 'block'
 let pageTwo = document.querySelector('#pageSecond').style.display = 'none'
-let pageThird = document.querySelector('#pageThird').style.display = 'block'
+let pageThird = document.querySelector('#pageThird').style.display = 'none'
 
-// const settingsPage = {
-//     transitionPagePrimary: function() {
-//         setTimeout(() => {
-//             pageOne.style.marginRight = '200%';
-//             setTimeout(() => {
-//                 pageTwo.style.display = "block";
-//             },500);
-//         }, 4900);                
-//     },
-//     transitionPageThird: function() {
-//         pageTwo.style.marginRight = '200%';
-//         setTimeout(() => {
-//             pageThird.style.display = "block";
-//         },500);
-//     },
-// };
+const settingsPage = {
+    transitionPagePrimary: function() {
+        setTimeout(() => {
+            pageOne.style.marginRight = '200%';
+            setTimeout(() => {
+                pageTwo.style.display = "block";
+            },500);
+        }, 4900);                
+    },
+    transitionPageThird: function() {
+        pageTwo.style.marginRight = '200%';
+        setTimeout(() => {
+            pageThird.style.display = "block";
+        },500);
+    },
+};
 
